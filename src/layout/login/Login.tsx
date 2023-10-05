5988bb6f866600e8fd679208b5c82d3f43fd9eef
RenShadecrest <michaelaaronphoto@gmail.com>
2023-10-04 17:35:45 -0700

    Completed Signup & Signin functionality

--- src/layout/login/Login.tsx
+++ src/layout/login/Login.tsx
@@ -1,43 +1,50 @@
-import { useState } from "react";
+import { useRef } from "react";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
 const Login: React.FC = () => {
   const auth = getAuth();
   const navigate = useNavigate();
-  const [ authing, setAuthing ] = useState(false);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
-  const signIn = async (email: string,password: string) => {
-    setAuthing(true);
+  const submitHandler = (e: React.FormEvent, type: string) => {
+    e.preventDefault();
+    const enteredEmail = emailRef.current!.value;
+    const enteredPassword = passwordRef.current!.value;
 
-    signInWithEmailAndPassword(auth, email, password)
-    .then((response) => {
-      console.log(response.user.uid);
-      navigate("/");
-    })
-    .catch((error) => {
-      console.error(error.code);
-      console.error(error.message);
-    });
-  };
+    if (type === "in") {
+      signInWithEmailAndPassword(auth, enteredEmail, enteredPassword)
+      .then((response) => {
+        console.log(response.user.uid);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error(error.code);
+        console.error(error.message);
+      });
+    } else if (type === "up"){
+      createUserWithEmailAndPassword(auth, enteredEmail, enteredPassword)
+      .then((response) => {
+        console.log(response.user.uid);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error(error.code);
+        console.error(error.message);
+      });
+    };
 
-  const signUp = async (email: string,password: string) => {
-    setAuthing(true);
-
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((response) => {
-      console.log(response.user.uid);
-      navigate("/");
-    })
-    .catch((error) => {
-      console.error(error.code);
-      console.error(error.message);
-    });
   };
 
   return (
     <>
-      Login Page
+      <form>
+        Email: <input type="text" ref={emailRef}/>
+        Password: <input type="text" ref={passwordRef}/>
+        <button type="submit" onClick={(e) => submitHandler(e, "in")}>Sign In</button>
+        <button type="submit" onClick={(e) => submitHandler(e, "up")}>Sign Up</button>
+      </form>
     </>
   )
 }
