026bd2367cb6059c2c20066a2a77e7c6c9c3d98e
RenShadecrest <michaelaaronphoto@gmail.com>
2023-10-06 14:54:29 -0700

    added header, adjusted theme colors, added nav control redux slice

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,12 @@
+import "./header.css";
+
+const Header: React.FC = () => {
+
+  return (
+    <>
+      <h1 className="header__title">Remazon Prime</h1>
+    </>
+  );
+};
+
+export default Header;
