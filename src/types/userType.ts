36c702845433e4f8e244c8c39233a8ff7057e4fd
RenShadecrest <michaelaaronphoto@gmail.com>
2023-10-25 14:47:35 -0700

    corrected types based on database updates

--- src/types/userType.ts
+++ src/types/userType.ts
@@ -2,14 +2,12 @@ type UserType = {
   id: number;
   name: string;
   uid: string;
-  employeeNumber: number | null;
   admin: boolean;
 };
 
 export type UserPostType = {
   name: string;
   uid: string;
-  employeeNumber: number | null;
-}
+};
 
 export default UserType;
