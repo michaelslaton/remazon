df09827319d9a49549948a550f7bf39e2d905504
RenShadecrest <michaelaaronphoto@gmail.com>
2023-12-11 13:24:07 -0800

    Adjusted styling for project, project display, employee display. Added character limits to descriptions

--- src/layout/projects/project-component/EditProject.tsx
+++ src/layout/projects/project-component/EditProject.tsx
@@ -1,30 +1,44 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import { editProjectThunk } from "../../../redux/slices/projectsSlice";
+import { setUiError } from "../../../redux/slices/controlsSlice";
 import ProjectType from "../../../types/projectType";
+import EmployeeType from "../../../types/employeeType";
 import "../projects.css";
 
 const EditProject: React.FC = () => {
   const { paramId } = useParams<string>();
+  const [ countData, setCountData ] = useState<number>(0);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
   const nameRef = useRef<HTMLInputElement>(null);
   const descriptionRef = useRef<HTMLTextAreaElement>(null);
   const statusRef = useRef<HTMLInputElement>(null);
   const hostRef = useRef<HTMLSelectElement>(null);
   const typeRef = useRef<HTMLInputElement>(null);
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
   const projects: ProjectType[] = useAppSelector((state)=> state.projectsControl.projects)
-  const selectedProject: ProjectType | undefined = projects.find((project)=> project.id === Number(paramId));  
+  const currentEmployee: EmployeeType | null = useAppSelector((state)=> state.employeesControl.currentEmployee);
+  const selectedProject: ProjectType | undefined = projects.find((project)=> project.id === Number(paramId));
+  const employeesList = useAppSelector((state)=> state.employeesControl.employees);
+
+  useEffect(()=> setCountData(selectedProject!.description.length),[]);
 
   const submitHandler: Function = (e: React.FormEvent): void => {
     e.preventDefault();
+    let inputHost = selectedProject!.host;
+
+    if (descriptionRef.current!.value.length > 200) {
+      dispatch(setUiError("Please shorten your description to 200 characters or less."));
+      return;
+    };
+    if (currentEmployee!.admin) inputHost = Number(hostRef.current!.value);
 
     const updatedProject: ProjectType = {
       ...selectedProject!,
       id: Number(paramId),
       name: nameRef.current!.value,
-      host: hostRef.current!.value,
+      host: inputHost,
       type: typeRef.current!.value,
       description: descriptionRef.current!.value,
       status: statusRef.current!.checked,
@@ -35,66 +49,76 @@ const EditProject: React.FC = () => {
   };
 
   return (
-    <>
-      <h2 className="title">Edit {selectedProject!.name}</h2>
-      <form className="project__edit-form">
-        
-        <label>
-          Name:
-          <input
-            type="text"
-            id="name"
-            name="name"
-            ref={nameRef}
-            defaultValue={selectedProject?.name}/>
-        </label>
+    <div className="center-display-space">
+      <div className="form-wrapper">
+        <h2 className="title">Edit {selectedProject!.name}</h2>
+        <form className="project__edit-form">
+          
+          <label>
+            <div className="form-input-label">Name:</div>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              ref={nameRef}
+              defaultValue={selectedProject?.name}/>
+          </label>
 
-        <label>
-          Host:
-          <select
-            id="host"
-            name="host"
-            ref={hostRef}
-            defaultValue={selectedProject?.host}>
-            <option value="3">wrong</option>
-            <option value="1">Right!</option>
-            <option value="2">wrong</option>
-          </select>
-        </label>
+          { currentEmployee!.admin &&
+            <label>
+              <div className="form-input-label">Host:</div>
+              <select
+                id="host"
+                name="host"
+                ref={hostRef}
+                defaultValue={selectedProject?.host}>
+                  {employeesList?.map((employee)=> (
+                    <option key={employee.id} value={employee.id}>{employee.name}</option>
+                  ))}
+              </select>
+            </label>
+          }
 
-        <label>
-          Type:
-          <input
-            type="text"
-            id="type"
-            name="type"
-            ref={typeRef}
-            defaultValue={selectedProject?.type}/>
-        </label>
+          <label>
+            <div className="form-input-label">Type:</div>
+            <input
+              type="text"
+              id="type"
+              name="type"
+              ref={typeRef}
+              defaultValue={selectedProject?.type}/>
+          </label>
 
-        <label>
-          Description:
-          <textarea
-            id="description"
-            name="description"
-            ref={descriptionRef}
-            defaultValue={selectedProject?.description}/>
-        </label>
+          <label>
+            <div className="form-input-label">Description:</div>
+            <textarea
+              id="description"
+              name="description"
+              ref={descriptionRef}
+              maxLength={200}
+              onChange={(e)=> setCountData(e.currentTarget.value.length)}
+              defaultValue={selectedProject?.description}/>
+          </label>
+          <div className="parameter-text">
+            {countData} of 200
+          </div>
 
-        <label>
-          Active:
-          <input
-            type="checkbox"
-            id="status"
-            name="status"
-            ref={statusRef}
-            defaultChecked={selectedProject?.status}/>
-        </label>
+          <label>
+            <div className="form-input-label">Active:</div>
+            <input
+              className="checkbox"
+              type="checkbox"
+              id="status"
+              name="status"
+              ref={statusRef}
+              defaultChecked={selectedProject?.status}/>
+          </label>
 
-        <button className="button project__edit-control" type="submit" onClick={(e)=> submitHandler(e)}>Submit</button>
-        <button className="button project__edit-control" onClick={()=> navigate("/projects")}>Cancel</button>
-      </form>
-    </>
+          <button className="button project__edit-control" type="submit" onClick={(e)=> submitHandler(e)}>Submit</button>
+          <button className="button project__edit-control" onClick={()=> navigate("/projects")}>Cancel</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
--- src/layout/projects/project-component/Project.tsx
+++ src/layout/projects/project-component/Project.tsx
@@ -19,7 +19,7 @@ const Project: React.FC<ProjectProps> = ({ data }) => {
   const [ showConfirm, setShowConfirm ] = useState<boolean>(false);
   const currentEmployee = useAppSelector((state)=> state.employeesControl.currentEmployee);
   const employees: EmployeeType[] = useAppSelector((state)=> state.employeesControl.employees);
-  const host: EmployeeType | undefined = employees.find((employee)=> employee.id === data.hostId);
+  const host: EmployeeType | undefined = employees.find((employee)=> employee.id === data.host);
   const ranks: RankType[] = useAppSelector((state)=> state.ranksControl.ranks);
   const currentHostsRank: RankType | undefined = ranks.find((rank)=> rank.id === host?.rank);
 
@@ -33,8 +33,8 @@ const Project: React.FC<ProjectProps> = ({ data }) => {
   // An Admin may always edit, otherwise employees can only edit their hosted projects if an admin has not locked it.
   const editButtonRender = (): ReactNode | null => {
     if ( data.locked && !currentEmployee?.admin ) return;
-    else if (currentEmployee?.admin || currentEmployee?.id === data.hostId) return (
-      <button className="button" onClick={()=> navigate(`/projects/edit/${data.id}`)}>
+    else if (currentEmployee?.admin || currentEmployee?.id === data.host) return (
+      <button className="button card-button" onClick={()=> navigate(`/projects/edit/${data.id}`)}>
         <FontAwesomeIcon icon={faEdit}/>
       </button>
     );
@@ -42,8 +42,8 @@ const Project: React.FC<ProjectProps> = ({ data }) => {
   
   const deleteButtonRender = (): ReactNode | null => {
     if ( data.locked && !currentEmployee?.admin ) return;
-    else if (currentEmployee?.admin || currentEmployee?.id === data.hostId) return (
-      <button className="button delete" onClick={()=> deleteButtonHandler()}>
+    else if (currentEmployee?.admin || currentEmployee?.id === data.host) return (
+      <button className="button delete card-button" onClick={()=> deleteButtonHandler()}>
         <FontAwesomeIcon icon={faTrashCan}/>
       </button>
     );
@@ -52,39 +52,41 @@ const Project: React.FC<ProjectProps> = ({ data }) => {
   return (
     <div className="project__wrapper">
       <h2 className="title project-title">{data.name}<div className={`status-dot ${data.status ? "active" : ""}`}/></h2>
-      <ul className="project__info">
+      <div className="project__info-wrapper">
+        <ul className="project__info">
 
-        <li>
-          <div className="project-data__key">
-            Host:
-          </div> 
-          <div className="project-data__value" style={{display: "inline", color: currentHostsRank?.color}}>
-            {data.host}
-          </div>
-        </li>
+          <li>
+            <div className="project-data__key">
+              Host:
+            </div> 
+            <div className="project-data__value" style={{display: "inline", color: currentHostsRank?.color}}>
+              {host!.name}
+            </div>
+          </li>
 
-        <li>
-          <div className="project-data__key">
-            Type:
-          </div>
-          <div className="project-data__value">
-            {data.type}
-          </div>
-        </li>
+          <li>
+            <div className="project-data__key">
+              Type:
+            </div>
+            <div className="project-data__value">
+              {data.type}
+            </div>
+          </li>
 
-        <li>
-          <div className="project-data__key">
-            Description:
-          </div>
-          <article className="project-data__description">
-            {data.description}
-          </article>
-        </li>
+          <li>
+            <div className="project-data__key">
+              Description:
+            </div>
+            <article className="project-data__description">
+              {data.description}
+            </article>
+          </li>
 
-      </ul>
-      <div className="project__edit-button_wrapper">
-        {deleteButtonRender()}
-        {editButtonRender()}
+        </ul>
+        <div className="project__buttons_wrapper">
+          {editButtonRender()}
+          {deleteButtonRender()}
+        </div>
       </div>
     </div>
   );
