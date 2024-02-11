d162efa27253e7ea89353bc8f39da703148c382e
RenShadecrest <michaelaaronphoto@gmail.com>
2024-02-10 18:23:06 -0800

    completed styling of route error component

--- src/utils/errors/route-error/RouteError.tsx
+++ src/utils/errors/route-error/RouteError.tsx
@@ -3,9 +3,15 @@ import './routeError.css';
 const RouteError: React.FC = () => {
 
   return (
-    <>
-      Something went wrong... Please refresh the page.
-    </>
+    <div className='route-error__wrapper'>
+      <div className='route-error__title--wrapper'>
+        <div className='route-error__title'>Something went wrong...</div>
+        <div className='route-error__text'>Please refresh the page...</div>
+        <div className='error404__links'>
+          <button className='error404-link' onClick={()=> location.reload()}>Refresh</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
