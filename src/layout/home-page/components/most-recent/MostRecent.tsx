3ec2d56003a8168d13ebdfdd6b5890a4d35f2ecd
RenShadecrest <michaelaaronphoto@gmail.com>
2024-03-03 17:23:46 -0800

    added hover animations for multiple items, added newest awarded to homepage, updated technologies

--- src/layout/home-page/components/most-recent/MostRecent.tsx
+++ src/layout/home-page/components/most-recent/MostRecent.tsx
@@ -1,13 +1,14 @@
 import MostRecentEmployee from './most-recent-employee/MostRecentEmployee';
 import MostRecentProject from './most-recent-project/MostRecentProject';
+import MostRecentAward from './most-recent-award/MostRecentAward';
 import './mostRecent.css';
 
 const MostRecent: React.FC = () => {
   return (
     <div className='most-recent__wrapper'>
       <MostRecentEmployee />
+      <MostRecentAward />
       <MostRecentProject />
-      <MostRecentEmployee />
     </div>
   );
 };
